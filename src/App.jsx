6e79057195b51dd1d13e23cc6967391f0dfbe564
Ali Moavia <alimoavia00123@gmail.com>
2025-10-30 04:21:40 +0500

    Fixed import path issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './component/Navbar'
-import Home from './component/home'
+import Home from './component/Home'
 import About from './component/About'
 import Skills from './component/Skills'
 import Portfolio from './component/Portfolio'
